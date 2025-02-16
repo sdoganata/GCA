3eb8890888eee2187b4d3b31416ee130826ed54a
sdoganata <81318317+sdoganata@users.noreply.github.com>
2025-02-16 22:20:48 +0300

    react-router-dom install

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Hero from '../ui components/Hero';
 import About from '../ui components/About';
-import HomeTitle from '../ui components/HomeTitle';
 
 const Home = ({ token }) => {
   return (
