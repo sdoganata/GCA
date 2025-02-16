94a783db8067f63396a7f9c4f674c348320e2ae2
sdoganata <81318317+sdoganata@users.noreply.github.com>
2025-02-16 23:00:15 +0300

    works properly, needs editing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
-import Navbar from './components/ui components/Navbar';
-import Footer from './components/ui components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/ui-components/Navbar';
+import Footer from './components/ui-components/Footer';
 import Home from './components/pages/Home';
 
 
 const App = () => {
     return (
         <div className='container'>
-        <Navbar/>
-        <Routes>
-            <Route path="/" element={<Home/>} />
-        </Routes>
-        <Footer/>
+            <Navbar/>
+            <Routes>
+                <Route path="/GCA/" element={<Home/>} />
+            </Routes>
+            <Footer/>
         </div>
     )
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Hero from '../ui components/Hero';
-import About from '../ui components/About';
+import Hero from '../ui-components/Hero';
+import About from '../ui-components/About';
 
 const Home = () => {
   return (
