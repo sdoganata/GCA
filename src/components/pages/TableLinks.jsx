6ea6b54505a5f7562091280e31b47bcc4f80717f
sdoganata <81318317+sdoganata@users.noreply.github.com>
2025-02-27 18:57:03 +0300

    fix links

--- src/components/pages/TableLinks.jsx
+++ src/components/pages/TableLinks.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const TableLinks = () => {
   return (
     <div className='page-div'>
-        <Link to='/browsetable' className='hero-link'>Cell-Line/Drug Pair Table</Link>
-        <Link to='/celllinetable' className='hero-link'>Cell-Line Table</Link>
-        <Link to='/drugtable' className='hero-link'>Drug Table</Link>
-        <Link to='/pdxtable' className='hero-link'>PDX Table</Link>
+        <Link to='/GCA/browsetable' className='hero-link'>Cell-Line/Drug Pair Table</Link>
+        <Link to='/GCA/celllinetable' className='hero-link'>Cell-Line Table</Link>
+        <Link to='/GCA/drugtable' className='hero-link'>Drug Table</Link>
+        <Link to='/GCA/pdxtable' className='hero-link'>PDX Table</Link>
     </div>
   )
 }
